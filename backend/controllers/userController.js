9822b829c29f63304408cc8e88b22fdeebb094a8
Ukaypromise <promiseukaimo@gmail.com>
2023-03-14 22:02:48 +0100

    Add registration and login for users with password validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,18 +1,64 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "User registered" });
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+  // Check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  //   Hash user password and create user
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  // Create user
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+  res.json({ message: "User Registered" });
 });
 
 // @desc    Authenticate  user
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "User Logged in" });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const decryptedPassword = await bcrypt.compare(password, user.password);
+  if (user && decryptedPassword) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentils");
+  }
 });
 
 // @desc    Get user info
